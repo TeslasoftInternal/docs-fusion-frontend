ae858d9cdf0b5da0482f50e0c6c8718cf3634fcb
AndraxDev <dostapenko82@gmail.com>
2024-11-24 01:41:12 +0100

    Save chat history

--- src/Pages/DocFusionAI.jsx
+++ src/Pages/DocFusionAI.jsx
@@ -6,23 +6,25 @@ import {runApi} from "../Services/Api";
 
 const DocFusionAI = ({func}) => {
 
-    const [userMessages, setUserMessages] = useState([]);
+    const [userMessages, setUserMessages] = useState(localStorage.getItem("messages") ? JSON.parse(localStorage.getItem("messages")) : []);
     const [file, setFile] = useState([])
     const [loading, setLoading] = useState(false)
 
     const pushMessage = (message) => {
-        if (message.message.trim() === "") return
         setUserMessages([...userMessages, message])
         sendMessages([...userMessages, message])
+        localStorage.setItem("messages", JSON.stringify([...userMessages, message]))
     }
 
     const pushMessageAsBot = (user, message) => {
         const msgs = [{type: "user", message: user}, {type: "ai", message: message}]
         setUserMessages([...userMessages, ...msgs])
+        localStorage.setItem("messages", JSON.stringify([...userMessages, ...msgs]))
     }
 
     const clearChat = () => {
         setUserMessages([])
+        localStorage.setItem("messages", JSON.stringify([]))
     }
 
     const sendMessages = (msgs) => {
